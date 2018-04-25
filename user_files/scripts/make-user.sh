03c6a2b93ee06b4e76d1c39de25c5fb840c3e3ef
Matthew Hemmings <discount.yoyos@gmail.com>
2018-04-25 11:00:14 -0700

    Move template files to their own directory

--- user_files/scripts/make-user.sh
+++ user_files/scripts/make-user.sh
@@ -13,8 +13,8 @@ openssl genrsa -out ../keys/$USER.key 2048
 openssl req -new -key ../keys/$USER.key -out ../keys/$USER.csr -subj "/CN=$USER/O=sundew"
 openssl x509 -req -in ../keys/$USER.csr -CA $CA_LOCATION/ca.crt -CAkey $CA_LOCATION/ca.key -CAcreateserial -out ../keys/$USER.crt -days 500
 kubectl config set-credentials $USER --client-certificate=../keys/$USER.crt  --client-key=../keys/$USER.key
-sed "s/template/$USER/g" ../yml/role-template.yml > ../yml/role-$USER.yml
-sed "s/template/$USER/g" ../yml/rb-template.yml > ../yml/rb-$USER.yml
+sed "s/template/$USER/g" ../yml/templates/role-template.yml > ../yml/role-$USER.yml
+sed "s/template/$USER/g" ../yml/templates/rb-template.yml > ../yml/rb-$USER.yml
 #kubectl create -f ../yml/role-$USER.yml
 #kubectl create -f ../yml/rb-$USER.yml
 #kubectl create rolebinding $USER-rolebind --clusterrole=admin --serviceaccount=$USER:default --namespace=$USER
