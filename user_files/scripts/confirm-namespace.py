f944eb6d63ba00cca60139e6c247f14066904b06
Rick McGeer <rick@mcgeer.com>
2018-05-08 14:05:51 -0700

    typo

--- user_files/scripts/confirm-namespace.py
+++ user_files/scripts/confirm-namespace.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     print >> sys.stderr, "Error: confirm-namespace.py takes exactly one argument, not %d" % len(sys.argv) - 1
     sys.exit(1)
   r = requests.post('http://sundewcluster.appspot.com/confirm_namespace', data = {'namespace':sys.argv[1]})
-  if r.status_code == requests.code.ok:
+  if r.status_code == requests.codes.ok:
     # do something with the response, which is a JSON dictionary with two fields: outcome (Success/Failure) and reason (a text string)
     pass
   else:
