88622c2c68c532006393349f4eefdfa551f84402
albert <albert.rafetseder+github@univie.ac.at>
2018-05-25 12:53:06 +0200

    Use spaces, not tabs for indentation
    Four spaces per indentation level, to be precise.

--- daemons/reg_daemon.py
+++ daemons/reg_daemon.py
@@ -15,51 +15,51 @@ autoDomainName = ''
 
 
 def mainRecords(hostRecords):
-	return filter(lambda x: x[2] != 'Type', hostRecords)
+    return filter(lambda x: x[2] != 'Type', hostRecords)
 
 
 
 def returnRecord(ip,name):
-	record = Record(ip,name)
-	return record
+    record = Record(ip,name)
+    return record
 
 def execCommand(anURL):
-	http = urllib3.PoolManager()
-	httpResponse = http.request('GET', anURL)
-	return  httpResponse.data
+    http = urllib3.PoolManager()
+    httpResponse = http.request('GET', anURL)
+    return  httpResponse.data
 
 
 def getHosts():
-	getHostsURL = '%s?Command=namecheap.domains.dns.getHosts&%s&%s&%s' % (host, authentication, domainInfo,clientIP)
-	xmlResult = parseString(execCommand(getHostsURL))
-	hostRecords = xmlResult.getElementsByTagName('host')
-	return  [(aRecord.getAttribute('Name'), aRecord.getAttribute('Address'), aRecord.getAttribute('Type')) for aRecord in hostRecords]
+    getHostsURL = '%s?Command=namecheap.domains.dns.getHosts&%s&%s&%s' % (host, authentication, domainInfo,clientIP)
+    xmlResult = parseString(execCommand(getHostsURL))
+    hostRecords = xmlResult.getElementsByTagName('host')
+    return  [(aRecord.getAttribute('Name'), aRecord.getAttribute('Address'), aRecord.getAttribute('Type')) for aRecord in hostRecords]
 
 def mainRecords(hostRecords):
-	return filter(lambda x: x[2] != 'Type', hostRecords)
+    return filter(lambda x: x[2] != 'Type', hostRecords)
 
 def makeSetHostURL(aHostList):
-	tuples = [(i + 1, aHostList[i][0], i + 1, aHostList[i][1], i + 1, aHostList[i][2]) for i in range(len(aHostList))]
-	hostStrings = ['HostName%d=%s&Address%d=%s&RecordType%d=%s' % aTuple for aTuple in tuples]
-	hostString = '&'.join(hostStrings)
-	setHostsURL = '%s?Command=namecheap.domains.dns.setHosts&%s&%s&%s&%s' % (host, authentication, domainInfo,clientIP,hostString)
-	return setHostsURL
+    tuples = [(i + 1, aHostList[i][0], i + 1, aHostList[i][1], i + 1, aHostList[i][2]) for i in range(len(aHostList))]
+    hostStrings = ['HostName%d=%s&Address%d=%s&RecordType%d=%s' % aTuple for aTuple in tuples]
+    hostString = '&'.join(hostStrings)
+    setHostsURL = '%s?Command=namecheap.domains.dns.setHosts&%s&%s&%s&%s' % (host, authentication, domainInfo,clientIP,hostString)
+    return setHostsURL
 
 #client = MongoClient('mongodb://mongodb:27017/')
 #db = client.gee_master
 #nodeCollection = db.nodes
 
 def hostsFromDB():
-	return execCommand('https://127.0.0.1:8080/nodes')
-	#nodes = nodeCollection.find({})
-	#nodes = filter(lambda x:x['dnsName'].endswith(autoDomainName), nodes)
-	#suffixLength = -len(autoDomainName)
-	#return [(node['dnsName'][:suffixLength], node['ipAddress'], 'A') for node in nodes]
+    return execCommand('https://127.0.0.1:8080/nodes')
+    #nodes = nodeCollection.find({})
+    #nodes = filter(lambda x:x['dnsName'].endswith(autoDomainName), nodes)
+    #suffixLength = -len(autoDomainName)
+    #return [(node['dnsName'][:suffixLength], node['ipAddress'], 'A') for node in nodes]
 
 def updateAll():
-	keepRecords = mainRecords(getHosts())
-	hostRecords = hostsFromDB()
-	setURL = makeSetHostURL(keepRecords + hostRecords)
-	return execCommand(set)
+    keepRecords = mainRecords(getHosts())
+    hostRecords = hostsFromDB()
+    setURL = makeSetHostURL(keepRecords + hostRecords)
+    return execCommand(set)
 
 
