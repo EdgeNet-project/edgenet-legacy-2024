7d89182434f130cace1dbc147d90b6095a77970f
Rick McGeer <rick@mcgeer.com>
2018-05-28 18:46:27 -0700

    removed namecheap_config.py

--- daemons/namecheap_config.py
+++
@@ -1,6 +0,0 @@
-# Authentication and configuration info for namecheap configuration.  DO NOT github!
-host = 'http://api.namecheap.com/xml.response'
-authentication = '613209274bb84fa7ae10f9d021a0e197'
-clientIP='142.104.71.105'
-ApiUser = 'rickmcgeer'
-autoDomainName = ''
