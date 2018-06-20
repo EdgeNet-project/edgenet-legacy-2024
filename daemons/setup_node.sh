d2ad8a53e65dbd73a071b76bc59ab8376fde4e51
Matt Hemmings <discount.yoyos@gmail.com>
2018-06-20 15:17:32 -0700

    Update setup_node to call portal instead of head node

--- daemons/setup_node.sh
+++ daemons/setup_node.sh
@@ -15,16 +15,17 @@ apt-get install docker.io kubelet kubeadm kubectl kubernetes-cni -y
 ##Right now, this is where the node is getting the secret straight from
 ##The head node.  THis will not work once we move to a whitelist access.
 ##It will need to move to the portal.
-SECRET=$(curl https://headnode.edge-net.org:8080/get_secret)
 
 swapoff -a
 read -p 'Enter site name (site.edge-net.io): ', sitename
+
+SECRET=$(curl https://sundewcluster.appspot.com/add_node?node_name=$sitename)
 
 ##This curl is where it was asking to join the cluster and passing its site name
 ##please change it to whatever you need for your new API
 ##curl https://headnode.edge-net.org:8080/add_node?sitename=$sitename
 ##
 OLDHOST=hostname
-hostname '$sitename.edge-net.io'
+hostname $sitename.edge-net.io
 $SECRET
 hostname $oldhost
