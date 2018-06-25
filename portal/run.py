3af470f586039f5f48d8678f9fa2158d7fb3b9fb
albert <albert.rafetseder+github@univie.ac.at>
2018-06-25 17:52:50 +0200

    Remove portal code, this is the headnode repo now!

--- portal/run.py
+++
@@ -1,5 +0,0 @@
-yourFolder = '/Users/rick/Projects/GoogleAppEngine/google-cloud-sdk/platform'
-sys.path.insert(1, yourFolder +'/google_appengine')
-sys.path.insert(1, yourFolder + '/google_appengine/lib/yaml/lib')
-if 'google' in sys.modules:           
-    del sys.modules['google']
