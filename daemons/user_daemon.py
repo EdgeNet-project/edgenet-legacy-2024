760292b4ceafc8f51ae7a22a3c60c477cac788b8
Rick McGeer <rick@mcgeer.com>
2018-11-14 22:30:14 -0800

    Added comments

--- daemons/user_daemon.py
+++ daemons/user_daemon.py
@@ -1,10 +1,44 @@
 #!/usr/bin/python
+"""
+user_daemon.py -- Interface between the EdgeNet portal and the headnode.  
+This implements a simple Flask server which handles requests from the EdgeNet 
+portal and issues commands to the headnode to implement them.  All authoritative state
+for the EdgeNet system is kept by the portal, which issues http requests through this server
+to ensure that the headnode is maintaining this state.
+
+This server also acts as an intermediary for DNS entries, currently kept by namecheap.com.  Namecheap
+uses IP whitelisting as its major security tool, and since the portal currently runs on the Google App
+Engine and this doesn't offer fixed IP addresses, we use this as the DNS intermediary.  Once we 
+either switch away from namecheap and/or switch to the Google App Engine Flex, we'll move that 
+to the portal.
+
+Copyright 2018 US Ignite
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This code uses the NYU style guidelines at:
+https://github.com/secure-systems-lab/code-style-guidelines 
+"""
 from flask import Flask, request, send_file, jsonify, Response
 import werkzeug # for proper 400 Bad Request handling
-import subprocess
+import subprocess # most portal requests turn into command executions of shell scripts
 from io import StringIO
-import namecheap_lib
+import namecheap_lib # for DNS
 from get_status import node_status
+
+#
+# What are these certs used for?
+#
 
 key = '/etc/letsencrypt/live/head.sundewproject.org/privkey.pem'
 cert = '/etc/letsencrypt/live/head.sundewproject.org/cert.pem'
@@ -18,7 +52,23 @@ class MissingRequestArgError(werkzeug.exceptions.BadRequest):
 def log(logString):
     print('[log]: ' + str(logString))
 
+
 def noblock_call(cmd, cwd="."):
+    """
+    <Purpose>
+      Issue cmd as a non-blocking shell command (in other words, issue the command and return immediately,
+      without waiting for a Returns)
+
+    <Arguments>
+      cmd: the command to issue
+      cwd: the directory to execute the command in.  Defaults to '.'
+
+    <Side Effects>
+      Executes the command
+
+    <Exceptions, Returns>
+      No Exceptions.  Returns an "Acknowledged" String if the process was forked successfully, and an error otherwise
+    """
     try:
       result = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE,
               bufsize=1, cwd=cwd)
@@ -27,6 +77,20 @@ def noblock_call(cmd, cwd="."):
       return "Error: " + repr(e)
 
 def make_call(cmd, cwd='.'):
+    """
+    <Purpose>
+      Issue cmd as a nblocking shell command (waits for the call to return, then returns the result)
+
+    <Arguments>
+      cmd: the command to issue
+      cwd: the directory to execute the command in.  Defaults to '.'
+
+    <Side Effects>
+      Executes the command
+
+    <Exceptions, Returns>
+      No Exceptions.  Returns the output if the command completed successfully, and an error otherwise
+    """
     try:
         result = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE,
                 bufsize=1, cwd=cwd).communicate()
@@ -43,6 +107,19 @@ def make_call(cmd, cwd='.'):
 #
 
 def remote_ip(request):
+    """
+    <Purpose>
+      Get the remote IP of a client that issued an http request
+
+    <Arguments>
+      request: the request, which should be an instance of flask.request
+
+    <Side Effects>
+      None
+
+    <Exceptions, Returns>
+      No Exceptions.  NOTE: should check if request is None.  returns the IP of the requestor.
+    """
     if 'X-Forwarded-For' in request.headers:
         request.headers.getlist("X-Forwarded-For")[0].rpartition(' ')[-1]
     else:
@@ -53,9 +130,27 @@ def remote_ip(request):
         return request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
 
 
+#
+# The execution engines for the various requests.
+#
 
 @app.route("/")
 def hello():
+    """
+    <Purpose>
+      Add a user to the headnode (actually, a namespace, which generates a configuration file)
+      Call is /?user=<username>
+      Question: I think this is now unused by the portal.  If it is it should be deleted.
+
+    <Arguments>
+      None
+
+    <Side Effects>
+      Creates the user and config file
+
+    <Exceptions, Returns>
+      raises a MissingRequestArgError if no parameter user in request.  Returns the result if successful; error otherwise
+    """
     if not "user" in request.args:
         raise MissingRequestArgError(description="No 'user' arg in request.")
 
@@ -73,6 +168,21 @@ def hello():
 
 @app.route('/make-user')
 def make_user():
+    """
+    <Purpose>
+      Add a user to the headnode (actually, a namespace, which generates a configuration file)
+      Call is /make-user?user=<username>
+
+    <Arguments>
+      None
+
+    <Side Effects>
+      Creates the user and config file
+
+    <Exceptions, Returns>
+      raises a MissingRequestArgError if no parameter user in request.  Returns a positive result from a successful non-blocking all if 
+      the process was invoked without error; error otherwise
+    """
     if not "user" in request.args:
         raise MissingRequestArgError(description="No 'user' arg in request.")
 
@@ -89,6 +199,21 @@ def make_user():
 
 @app.route("/nodes")
 def get_nodes():
+    """
+    <Purpose>
+      Get the list of nodes currently known by the head node
+      Call is /nodes
+
+    <Arguments>
+      None
+
+    <Side Effects>
+      None
+
+    <Exceptions, Returns>
+      returns the list of nodes as a JSON file (see the get_nodes shell script), or error if there was an error invoking
+      the get_nodes script
+    """
     try:
       cmd = ['./get_nodes.sh']
       log(cmd)
@@ -100,10 +225,39 @@ def get_nodes():
 
 @app.route("/get_status")
 def get_status():
+    """
+    <Purpose>
+      Get the status of nodes currently known by the head node
+      Call is /get_status
+
+    <Arguments>
+      None
+
+    <Side Effects>
+      None
+
+    <Exceptions, Returns>
+      See Exceptions, Returns from get_status.node_status
+    """
     return node_status()
 
 @app.route("/get_secret")
 def get_secret():
+    """
+    <Purpose>
+      Get a shared secret that the add_node script can run on the client and pass to this node to ensure that
+      the add_node is legitimate
+      Call is /get_secret
+
+    <Arguments>
+      None
+
+    <Side Effects>
+      None
+
+    <Exceptions, Returns>
+      Returns the result of the command $ sudo kubeadm token create --print-join-command --ttl 30s
+    """
     cmd = ['sudo', 'kubeadm', 'token', 'create', '--print-join-command','--ttl', '30s']
     log ('token request')
     result = make_call(cmd)[0]
@@ -112,6 +266,20 @@ def get_secret():
 
 @app.route("/show_ip")
 def show_ip():
+    """
+    <Purpose>
+      Test the remote_ip() routine: return the IP of the requestor
+      Call: /show_ip
+
+    <Arguments>
+      None
+
+    <Side Effects>
+      None
+
+    <Exceptions, Returns>
+      Returns the IP of the requesting host
+    """
    return 'Your request is from %s' % remote_ip(request)
 #
 # /add_node?sitename=<node_name>&ip_address=address
@@ -119,6 +287,20 @@ def show_ip():
 #
 @app.route("/add_node")
 def add_node():
+    """
+    <Purpose>
+      Add a node to the DNS records for edge-net.io
+      Call: /add_node?ip_address=<ip_address>&sitename=<sitename>&record_type=<A or AAA>
+
+    <Arguments>
+      None
+
+    <Side Effects>
+      Adds the record (sitename.edge-net.io, ip_address, record_type) to the DB on namecheap
+
+    <Exceptions, Returns>
+      Either a successful we-added-it or an error return if the sitename or ip address already exists
+    """
     ip = str(request.args.get('ip_address'))
     site = str(request.args.get('sitename'))
     record_type = str(request.args.get('record_type'))
@@ -133,14 +315,56 @@ def add_node():
 
 @app.route("/get_setup")
 def get_setup():
+    """
+    <Purpose>
+      Download the setup_node.sh script
+      Call: /get_setup
+
+    <Arguments>
+      None
+
+    <Side Effects>
+      None
+
+    <Exceptions, Returns>
+      Sends the setup_node.sh file
+    """
     return send_file('setup_node.sh')
 
 @app.route("/show_headers")
 def get_headers():
+    """
+    <Purpose>
+      Show all the headers, just for testing purposes
+      Call: /show_headers
+
+    <Arguments>
+      None
+
+    <Side Effects>
+      None
+
+    <Exceptions, Returns>
+      Returns the headers of the request
+    """
     return jsonify(request.headers)
 
 @app.route("/namespaces")
 def get_namespaces():
+     """
+    <Purpose>
+      Get the set of current namespaces
+      Call: /get_namespaces
+
+    <Arguments>
+      None
+
+    <Side Effects>
+      None
+
+    <Exceptions, Returns>
+      Returns the current namespaces
+    """
   cmd = ['./get-namespaces.sh']
   return make_call(cmd)
 
