5ef177618be9eeab902e8603636df45e4765948d
Berat Senel <38038754+bsenel@users.noreply.github.com>
2020-04-30 16:58:02 +0200

    Revert "Update SRR, URR, and SD for minor improvements"
    This reverts commit c7b799ca0c53bb86870cd683a30f654652f3d851.

--- pkg/controller/v1alpha/siteregistrationrequest/controller.go
+++ pkg/controller/v1alpha/siteregistrationrequest/controller.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2020 Sorbonne Université
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package siteregistrationrequest
 
 import (
@@ -49,7 +33,7 @@ type informerevent struct {
 	function string
 }
 
-// Constant variables for events
+// Definitions of the siteregistrationrequest CRD
 const create = "create"
 const update = "update"
 const delete = "delete"
@@ -88,6 +72,7 @@ func Start() {
 			if !reflect.DeepEqual(oldObj.(*apps_v1alpha.SiteRegistrationRequest).Status, newObj.(*apps_v1alpha.SiteRegistrationRequest).Status) {
 				event.key, err = cache.MetaNamespaceKeyFunc(newObj)
 				event.function = update
+
 				log.Infof("Update siteregistrationrequest: %s", event.key)
 				if err == nil {
 					queue.Add(event)
