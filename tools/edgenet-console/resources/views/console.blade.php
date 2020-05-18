d86e47616558c134db2acdcb2f813631bd87a02a
Ciro Scognamiglio <c.scognamiglio@cslash.net>
2020-05-18 12:24:31 +0200

    Kubernetes webhook authentication

--- tools/edgenet-console/resources/views/console.blade.php
+++ tools/edgenet-console/resources/views/console.blade.php
@@ -8,10 +8,10 @@
     <title>{{ env('APP_NAME', 'EdgeNet Console') }}</title>
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{ mix('css/flag-icon.min.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}" />
+{{--    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}" />--}}
     <script type="text/javascript" src="{{ mix('js/app.js') }}" defer></script>
 </head>
 <body>
-<div id="edgenet-console"></div>
+<div id="application"></div>
 </body>
 </html>
