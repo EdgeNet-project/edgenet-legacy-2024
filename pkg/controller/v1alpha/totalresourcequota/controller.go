2e620689b0985b61e021cf0c9a8093dcccc2ad4c
bsenel <beratcansenel@gmail.com>
2020-05-20 20:03:37 +0200

    Add reward system including node watching

--- pkg/controller/v1alpha/totalresourcequota/controller.go
+++ pkg/controller/v1alpha/totalresourcequota/controller.go
@@ -23,12 +23,19 @@ import (
 	"syscall"
 	"time"
 
+	"edgenet/pkg/apis/apps/v1alpha"
 	"edgenet/pkg/authorization"
 	appsinformer_v1 "edgenet/pkg/client/informers/externalversions/apps/v1alpha"
+	"edgenet/pkg/node"
 
 	log "github.com/Sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 )
@@ -53,9 +60,17 @@ const update = "update"
 const delete = "delete"
 const failure = "Pulled off"
 const success = "Applied"
+const trueStr = "True"
+const falseStr = "False"
+const unknownStr = "Unknown"
 
 // Start function is entry point of the controller
 func Start() {
+	clientset, err := authorization.CreateClientSet()
+	if err != nil {
+		log.Println(err.Error())
+		panic(err.Error())
+	}
 	edgenetClientset, err := authorization.CreateEdgeNetClientSet()
 	if err != nil {
 		log.Println(err.Error())
@@ -100,6 +115,169 @@ func Start() {
 			log.Infof("Delete TRQ: %s", event.key)
 			if err == nil {
 				queue.Add(event)
+			}
+		},
+	})
+	// The selectivedeployment resources are reconfigured according to node events in this section
+	nodeInformer := cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			// The main purpose of listing is to attach geo labels to whole nodes at the beginning
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return clientset.CoreV1().Nodes().List(options)
+			},
+			// This function watches all changes/updates of nodes
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return clientset.CoreV1().Nodes().Watch(options)
+			},
+		},
+		&corev1.Node{},
+		0,
+		cache.Indexers{},
+	)
+	nodeInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			nodeObj := obj.(*corev1.Node)
+			for key, _ := range nodeObj.Labels {
+				if key == "node-role.kubernetes.io/master" {
+					return
+				}
+			}
+			ready := node.GetConditionReadyStatus(nodeObj)
+			if ready == trueStr {
+				for _, owner := range nodeObj.GetOwnerReferences() {
+					if owner.Kind == "Authority" {
+						TRQCopy, err := edgenetClientset.AppsV1alpha().TotalResourceQuotas().Get(owner.Name, metav1.GetOptions{})
+						if err == nil {
+							exists := false
+							CPUAward := resource.NewQuantity(int64(float64(nodeObj.Status.Capacity.Cpu().Value())*1.3), resource.BinarySI).DeepCopy()
+							memoryAward := resource.NewQuantity(int64(float64(nodeObj.Status.Capacity.Memory().Value())*1.3), resource.BinarySI).DeepCopy()
+							claims := TRQCopy.Spec.Claim
+							for i, claimRow := range TRQCopy.Spec.Claim {
+								if claimRow.Name == "Reward" {
+									exists = true
+									rewardedCPU := resource.MustParse(claimRow.CPU)
+									rewardedCPU.Add(CPUAward)
+									rewardedMemory := resource.MustParse(claimRow.Memory)
+									rewardedMemory.Add(memoryAward)
+									claimRow.CPU = CPUAward.String()
+									claimRow.Memory = memoryAward.String()
+									claims = append(claims[:i], claims[i+1:]...)
+									claims = append(claims, claimRow)
+								}
+							}
+							if !exists {
+								claim := v1alpha.TotalResourceDetails{}
+								claim.Name = "Reward"
+								claim.CPU = CPUAward.String()
+								claim.Memory = memoryAward.String()
+								TRQCopy.Spec.Claim = append(TRQCopy.Spec.Claim, claim)
+							} else {
+								TRQCopy.Spec.Claim = claims
+							}
+							edgenetClientset.AppsV1alpha().TotalResourceQuotas().Update(TRQCopy)
+						}
+					}
+				}
+			}
+		},
+		UpdateFunc: func(old, new interface{}) {
+			oldObj := old.(*corev1.Node)
+			newObj := new.(*corev1.Node)
+			oldReady := node.GetConditionReadyStatus(oldObj)
+			newReady := node.GetConditionReadyStatus(newObj)
+			if (oldReady == falseStr && newReady == trueStr) ||
+				(oldReady == unknownStr && newReady == trueStr) {
+				for _, owner := range newObj.GetOwnerReferences() {
+					if owner.Kind == "Authority" {
+						TRQCopy, err := edgenetClientset.AppsV1alpha().TotalResourceQuotas().Get(owner.Name, metav1.GetOptions{})
+						if err == nil {
+							exists := false
+							CPUAward := resource.NewQuantity(int64(float64(newObj.Status.Capacity.Cpu().Value())*1.3), resource.BinarySI).DeepCopy()
+							memoryAward := resource.NewQuantity(int64(float64(newObj.Status.Capacity.Memory().Value())*1.3), resource.BinarySI).DeepCopy()
+							claims := TRQCopy.Spec.Claim
+							for i, claimRow := range TRQCopy.Spec.Claim {
+								if claimRow.Name == "Reward" {
+									exists = true
+									rewardedCPU := resource.MustParse(claimRow.CPU)
+									rewardedCPU.Add(CPUAward)
+									rewardedMemory := resource.MustParse(claimRow.Memory)
+									rewardedMemory.Add(memoryAward)
+									claimRow.CPU = CPUAward.String()
+									claimRow.Memory = memoryAward.String()
+									claims = append(claims[:i], claims[i+1:]...)
+									claims = append(claims, claimRow)
+								}
+							}
+							if !exists {
+								claim := v1alpha.TotalResourceDetails{}
+								claim.Name = "Reward"
+								claim.CPU = CPUAward.String()
+								claim.Memory = memoryAward.String()
+								TRQCopy.Spec.Claim = append(TRQCopy.Spec.Claim, claim)
+							} else {
+								TRQCopy.Spec.Claim = claims
+							}
+							edgenetClientset.AppsV1alpha().TotalResourceQuotas().Update(TRQCopy)
+						}
+					}
+				}
+			} else if (oldReady == trueStr && newReady == falseStr) ||
+				(oldReady == trueStr && newReady == unknownStr) {
+				for _, owner := range newObj.GetOwnerReferences() {
+					if owner.Kind == "Authority" {
+						TRQCopy, err := edgenetClientset.AppsV1alpha().TotalResourceQuotas().Get(owner.Name, metav1.GetOptions{})
+						if err == nil {
+							CPUAward := resource.NewQuantity(int64(float64(newObj.Status.Capacity.Cpu().Value())*1.3), resource.BinarySI).DeepCopy()
+							memoryAward := resource.NewQuantity(int64(float64(newObj.Status.Capacity.Memory().Value())*1.3), resource.BinarySI).DeepCopy()
+							claims := TRQCopy.Spec.Claim
+							for i, claimRow := range TRQCopy.Spec.Claim {
+								if claimRow.Name == "Reward" {
+									rewardedCPU := resource.MustParse(claimRow.CPU)
+									rewardedCPU.Sub(CPUAward)
+									rewardedMemory := resource.MustParse(claimRow.Memory)
+									rewardedMemory.Sub(memoryAward)
+									claimRow.CPU = CPUAward.String()
+									claimRow.Memory = memoryAward.String()
+									claims = append(claims[:i], claims[i+1:]...)
+									claims = append(claims, claimRow)
+								}
+							}
+							TRQCopy.Spec.Claim = claims
+							edgenetClientset.AppsV1alpha().TotalResourceQuotas().Update(TRQCopy)
+						}
+					}
+				}
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			log.Println("Node Deleted Event")
+			nodeObj := obj.(*corev1.Node)
+			ready := node.GetConditionReadyStatus(nodeObj)
+			if ready == trueStr {
+				for _, owner := range nodeObj.GetOwnerReferences() {
+					if owner.Kind == "Authority" {
+						TRQCopy, err := edgenetClientset.AppsV1alpha().TotalResourceQuotas().Get(owner.Name, metav1.GetOptions{})
+						if err == nil {
+							CPUAward := resource.NewQuantity(int64(float64(nodeObj.Status.Capacity.Cpu().Value())*1.3), resource.BinarySI).DeepCopy()
+							memoryAward := resource.NewQuantity(int64(float64(nodeObj.Status.Capacity.Memory().Value())*1.3), resource.BinarySI).DeepCopy()
+							claims := TRQCopy.Spec.Claim
+							for i, claimRow := range TRQCopy.Spec.Claim {
+								if claimRow.Name == "Reward" {
+									rewardedCPU := resource.MustParse(claimRow.CPU)
+									rewardedCPU.Sub(CPUAward)
+									rewardedMemory := resource.MustParse(claimRow.Memory)
+									rewardedMemory.Sub(memoryAward)
+									claimRow.CPU = CPUAward.String()
+									claimRow.Memory = memoryAward.String()
+									claims = append(claims[:i], claims[i+1:]...)
+									claims = append(claims, claimRow)
+								}
+							}
+							TRQCopy.Spec.Claim = claims
+							edgenetClientset.AppsV1alpha().TotalResourceQuotas().Update(TRQCopy)
+						}
+					}
+				}
 			}
 		},
 	})
