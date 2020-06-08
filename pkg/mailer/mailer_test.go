19f29512259376937075ce40d2d86fa085beced5
SAzouaou <66604494+SAzouaou@users.noreply.github.com>
2020-06-08 12:30:07 +0200

    Add files via upload

---
+++ pkg/mailer/mailer_test.go
@@ -0,0 +1,45 @@
+package mailer
+import (
+	
+	"testing"
+	"strings"
+	"regexp"
+	"fmt"
+)
+func TestGenerateRandomString(t *testing.T) {
+
+	var codes []string
+
+  for i := 1; i <= 100; i++ {
+		task := generateRandomString(10)
+		if len(task) != 10{
+		    t.Errorf("code %d has wrong length", len(task))
+		}
+	//string unique
+  if (len(codes) != 0){
+    for _,code := range codes{
+      if(strings.Compare(task, code )) == 0 {
+          t.Errorf("duplicate code %s received", task)
+      }
+    }
+  }
+	codes = append(codes, task)
+
+  // if string
+		var IsLetter = regexp.MustCompile(`^[a-zA-Z]+$`).MatchString
+
+	if(!IsLetter(task)){
+	   t.Errorf("Not string code %s received", task)
+	}
+}
+}
+/*
+func TestSetCommonEmailHeaders(t *testing.T){
+	
+ 	setCommonEmailHeaders("sujet", "meli", []string{"samia"}, "delimiter")
+ 
+}
+*/
+
+
+
