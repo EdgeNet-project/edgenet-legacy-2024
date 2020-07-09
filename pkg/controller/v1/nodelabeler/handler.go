7c07470586acd618c21e1d3ac863db71c7ead91f
bsenel <beratcansenel@gmail.com>
2020-07-09 20:30:21 +0200

    Update the nodelabeler package for bugfixing

--- pkg/controller/v1/nodelabeler/handler.go
+++ pkg/controller/v1/nodelabeler/handler.go
@@ -1,10 +1,12 @@
 package nodelabeler
 
 import (
+	"edgenet/pkg/bootstrap"
 	"edgenet/pkg/node"
-	
+
 	log "github.com/Sirupsen/logrus"
 	api_v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 // HandlerInterface interface contains the methods that are required
@@ -14,11 +16,20 @@ type HandlerInterface interface {
 }
 
 // Handler is a sample implementation of Handler
-type Handler struct{}
+type Handler struct {
+	clientset *kubernetes.Clientset
+}
 
 // Init handles any handler initialization
 func (t *Handler) Init() error {
 	log.Info("Handler.Init")
+	var err error
+	t.clientset, err = bootstrap.CreateClientSet()
+	if err != nil {
+		log.Println(err.Error())
+		panic(err.Error())
+	}
+	node.Clientset = t.clientset
 	return nil
 }
 
@@ -26,14 +37,14 @@ func (t *Handler) Init() error {
 func (t *Handler) SetNodeGeolocation(obj interface{}) {
 	log.Info("Handler.ObjectCreated")
 	// Get internal and external IP addresses of the node
-	internalIP, externalIP := node.GetNodeIPAddresses(obj.(*api_v1.Node))	
+	internalIP, externalIP := node.GetNodeIPAddresses(obj.(*api_v1.Node))
 	result := false
 	// Check if the external IP exists to use it in the first place
 	if externalIP != "" {
 		log.Infof("External IP: %s", externalIP)
 		result = node.GetGeolocationByIP(obj.(*api_v1.Node).Name, externalIP)
 	}
-	// Check if the internal IP exists and 
+	// Check if the internal IP exists and
 	// the result of detecting geolocation by external IP is false
 	if internalIP != "" && result == false {
 		log.Infof("Internal IP: %s", internalIP)
