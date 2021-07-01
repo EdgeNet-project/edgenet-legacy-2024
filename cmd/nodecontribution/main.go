2d5b7be4aa87ce40e0be418d5b7cb30b497f08bd
bsenel <beratcansenel@gmail.com>
2021-07-01 15:18:24 +0200

    Update the main file of node contribution

--- cmd/nodecontribution/main.go
+++ cmd/nodecontribution/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/EdgeNet-project/edgenet/pkg/bootstrap"
 	"github.com/EdgeNet-project/edgenet/pkg/controller/core/v1alpha/nodecontribution"
+	informers "github.com/EdgeNet-project/edgenet/pkg/generated/informers/externalversions"
+	"github.com/EdgeNet-project/edgenet/pkg/signals"
+
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/klog"
 )
 
 func main() {
+	stopCh := signals.SetupSignalHandler()
 	kubeclientset, err := bootstrap.CreateClientset("serviceaccount")
 	if err != nil {
 		log.Println(err.Error())
@@ -19,5 +26,18 @@ func main() {
 		panic(err.Error())
 	}
 	// Start the controller to provide the functionalities of nodecontribution resource
-	nodecontribution.Start(clientset, edgenetClientset)
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeclientset, time.Second*30)
+	edgenetInformerFactory := informers.NewSharedInformerFactory(edgenetclientset, time.Second*30)
+
+	controller := nodecontribution.NewController(kubeclientset,
+		edgenetclientset,
+		kubeInformerFactory.Core().V1().Nodes(),
+		edgenetInformerFactory.Core().V1alpha().NodeContributions())
+
+	kubeInformerFactory.Start(stopCh)
+	edgenetInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
 }
