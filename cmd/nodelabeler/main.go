60f26ffd5fb3fe23eaf73575c2f104fc61a3bc58
Ufuk Bombar <ufukbombar@gmail.com>
2021-09-13 18:10:28 +0300

    Updated Nodelabeler Controller to Match with the Sample-Controller (#157)
    * Fixed node labeler attempt 1
    
    * Update Nodelabeler Controller
    
    Note that there are still issues that needs to be checked
    
    * Add main, test and controller fix
    
    * Remove nodelabeler handler
    
    * Remove edgenetclientset from nodelabeler
    
    * Fix test cases
    
    * Change kubeconfig to serviceaccount
    
    * Fixed edgenetscheme issue
    
    * Fix typo and test
    
    * Add enqueue func
    
    * Fix typo
    
    * Fix typo
    
    * Change log to klog
    
    * Add new line
    
    * Add nodelabeler database test path
    
    Co-authored-by: Ufuk Bombar <ufuk.bombar@bordatech.com>

--- cmd/nodelabeler/main.go
+++ cmd/nodelabeler/main.go
@@ -21,12 +21,17 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/EdgeNet-project/edgenet/pkg/bootstrap"
 	"github.com/EdgeNet-project/edgenet/pkg/controller/apps/v1/nodelabeler"
+	"github.com/EdgeNet-project/edgenet/pkg/signals"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/klog"
 )
 
 func main() {
+	stopCh := signals.SetupSignalHandler()
 	// TODO: Pass an argument to select using kubeconfig or service account for clients
 	// bootstrap.SetKubeConfig()
 	kubeclientset, err := bootstrap.CreateClientset("serviceaccount")
@@ -34,6 +39,16 @@ func main() {
 		log.Println(err.Error())
 		panic(err.Error())
 	}
-	// Start the controller to watch nodes and attach the labels to them
-	nodelabeler.Start(kubeclientset)
+
+	// Start the controller to provide the functionalities of nodelabeler resource
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeclientset, time.Second*30)
+
+	controller := nodelabeler.NewController(kubeclientset,
+		kubeInformerFactory.Core().V1().Nodes())
+
+	kubeInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
 }
