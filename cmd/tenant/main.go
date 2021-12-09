908147aadd6da6bc0c6e1daab6c1678d103461f4
Ufuk Bombar <ufuk.bombar@bordatech.com>
2021-12-09 19:05:57 +0300

    Change tenant main

--- cmd/tenant/main.go
+++ cmd/tenant/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"flag"
-	"k8s.io/klog"
 	"log"
+	"time"
+
+	"k8s.io/klog"
 
 	"github.com/EdgeNet-project/edgenet/pkg/bootstrap"
 	"github.com/EdgeNet-project/edgenet/pkg/controller/core/v1alpha/tenant"
+	"github.com/EdgeNet-project/edgenet/pkg/signals"
+
+	informers "github.com/EdgeNet-project/edgenet/pkg/generated/informers/externalversions"
+	kubeinformers "k8s.io/client-go/informers"
 )
 
 func main() {
 	klog.InitFlags(nil)
 	flag.Parse()
 
+	stopCh := signals.SetupSignalHandler()
 	// TODO: Pass an argument to select using kubeconfig or service account for clients
 	// bootstrap.SetKubeConfig()
 	kubeclientset, err := bootstrap.CreateClientset("serviceaccount")
@@ -25,6 +32,18 @@ func main() {
 		log.Println(err.Error())
 		panic(err.Error())
 	}
-	// Start the controller to provide the functionalities of tenant resource
-	tenant.Start(kubeclientset, edgenetclientset)
+	// Start the controller to provide the functionalities of nodecontribution resource
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeclientset, time.Second*30)
+	edgenetInformerFactory := informers.NewSharedInformerFactory(edgenetclientset, 0)
+
+	controller := tenant.NewController(kubeclientset,
+		edgenetclientset,
+		edgenetInformerFactory.Core().V1alpha().Tenants())
+
+	kubeInformerFactory.Start(stopCh)
+	edgenetInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
 }
