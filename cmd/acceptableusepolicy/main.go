0f3dcbe59c0ab3e99d70e308f8b04dfd891aab2c
bsenel <beratcansenel@gmail.com>
2021-12-12 12:04:05 +0100

    Update AUP to fit in new controller model

--- cmd/acceptableusepolicy/main.go
+++ cmd/acceptableusepolicy/main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"flag"
-	"k8s.io/klog"
 	"log"
 
 	"github.com/EdgeNet-project/edgenet/pkg/bootstrap"
 	"github.com/EdgeNet-project/edgenet/pkg/controller/core/v1alpha/acceptableusepolicy"
+	informers "github.com/EdgeNet-project/edgenet/pkg/generated/informers/externalversions"
+	"github.com/EdgeNet-project/edgenet/pkg/signals"
+
+	"k8s.io/klog"
 )
 
 func main() {
 	klog.InitFlags(nil)
 	flag.Parse()
 
+	stopCh := signals.SetupSignalHandler()
 	// TODO: Pass an argument to select using kubeconfig or service account for clients
 	// bootstrap.SetKubeConfig()
 	kubeclientset, err := bootstrap.CreateClientset("serviceaccount")
@@ -26,5 +30,15 @@ func main() {
 		panic(err.Error())
 	}
 	// Start the controller to provide the functionalities of acceptableusepolicy resource
-	acceptableusepolicy.Start(kubeclientset, edgenetclientset)
+	edgenetInformerFactory := informers.NewSharedInformerFactory(edgenetclientset, 0)
+
+	controller := acceptableusepolicy.NewController(kubeclientset,
+		edgenetclientset,
+		edgenetInformerFactory.Core().V1alpha().AcceptableUsePolicies())
+
+	edgenetInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
 }
