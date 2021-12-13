a7a0af6c022cc633e1aead8940bae2ebd19b88c7
bsenel <beratcansenel@gmail.com>
2021-12-13 10:44:18 +0100

    Update Email Verification to fit in new controller model

--- cmd/emailverification/main.go
+++ cmd/emailverification/main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"flag"
-	"k8s.io/klog"
 	"log"
 
 	"github.com/EdgeNet-project/edgenet/pkg/bootstrap"
 	"github.com/EdgeNet-project/edgenet/pkg/controller/registration/v1alpha/emailverification"
+	informers "github.com/EdgeNet-project/edgenet/pkg/generated/informers/externalversions"
+	"github.com/EdgeNet-project/edgenet/pkg/signals"
+
+	"k8s.io/klog"
 )
 
 func main() {
 	klog.InitFlags(nil)
 	flag.Parse()
 
+	stopCh := signals.SetupSignalHandler()
 	// TODO: Pass an argument to select using kubeconfig or service account for clients
 	// bootstrap.SetKubeConfig()
 	kubeclientset, err := bootstrap.CreateClientset("serviceaccount")
@@ -26,5 +30,15 @@ func main() {
 		panic(err.Error())
 	}
 	// Start the controller to provide the functionalities of emailverification resource
-	emailverification.Start(kubeclientset, edgenetclientset)
+	edgenetInformerFactory := informers.NewSharedInformerFactory(edgenetclientset, 0)
+
+	controller := emailverification.NewController(kubeclientset,
+		edgenetclientset,
+		edgenetInformerFactory.Registration().V1alpha().EmailVerifications())
+
+	edgenetInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
 }
