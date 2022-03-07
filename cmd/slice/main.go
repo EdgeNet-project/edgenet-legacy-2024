2777689d729f74e247a61f3c458f6904ff41c801
bsenel <beratcansenel@gmail.com>
2022-03-07 13:53:37 +0100

    Add Slice and SliceClaim to the build matrix

---
+++ cmd/slice/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	"github.com/EdgeNet-project/edgenet/pkg/bootstrap"
+	"github.com/EdgeNet-project/edgenet/pkg/controller/core/v1alpha/slice"
+	informers "github.com/EdgeNet-project/edgenet/pkg/generated/informers/externalversions"
+	"github.com/EdgeNet-project/edgenet/pkg/signals"
+
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/klog"
+)
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	stopCh := signals.SetupSignalHandler()
+	// TODO: Pass an argument to select using kubeconfig or service account for clients
+	// bootstrap.SetKubeConfig()
+	kubeclientset, err := bootstrap.CreateClientset("serviceaccount")
+	if err != nil {
+		log.Println(err.Error())
+		panic(err.Error())
+	}
+	edgenetclientset, err := bootstrap.CreateEdgeNetClientset("serviceaccount")
+	if err != nil {
+		log.Println(err.Error())
+		panic(err.Error())
+	}
+	// Start the controller to provide the functionalities of slice resource
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeclientset, time.Second*30)
+	edgenetInformerFactory := informers.NewSharedInformerFactory(edgenetclientset, 0)
+
+	controller := slice.NewController(kubeclientset,
+		edgenetclientset,
+		edgenetInformerFactory.Core().V1alpha().SliceClaims(),
+		edgenetInformerFactory.Core().V1alpha().Slices())
+
+	kubeInformerFactory.Start(stopCh)
+	edgenetInformerFactory.Start(stopCh)
+
+	if err = controller.Run(1, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
+}
---
+++ cmd/sliceclaim/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/EdgeNet-project/edgenet/pkg/bootstrap"
+	"github.com/EdgeNet-project/edgenet/pkg/controller/core/v1alpha/sliceclaim"
+	informers "github.com/EdgeNet-project/edgenet/pkg/generated/informers/externalversions"
+	"github.com/EdgeNet-project/edgenet/pkg/signals"
+
+	"k8s.io/klog"
+)
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	stopCh := signals.SetupSignalHandler()
+	// TODO: Pass an argument to select using kubeconfig or service account for clients
+	// bootstrap.SetKubeConfig()
+	kubeclientset, err := bootstrap.CreateClientset("serviceaccount")
+	if err != nil {
+		log.Println(err.Error())
+		panic(err.Error())
+	}
+	edgenetclientset, err := bootstrap.CreateEdgeNetClientset("serviceaccount")
+	if err != nil {
+		log.Println(err.Error())
+		panic(err.Error())
+	}
+	// Start the controller to provide the functionalities of sliceclaim resource
+	edgenetInformerFactory := informers.NewSharedInformerFactory(edgenetclientset, 0)
+
+	controller := sliceclaim.NewController(kubeclientset,
+		edgenetclientset,
+		edgenetInformerFactory.Core().V1alpha().SubNamespaces(),
+		edgenetInformerFactory.Core().V1alpha().SliceClaims())
+
+	edgenetInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
+}
