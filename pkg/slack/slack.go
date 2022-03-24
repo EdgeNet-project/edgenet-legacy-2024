b6c22eada7ddc4c0c30843e8914a567896a884f2
ubombar <ufukbombar@gmail.com>
2022-03-24 17:36:30 +0300

    Add dummy kubectl commands

--- pkg/slack/slack.go
+++ pkg/slack/slack.go
@@ -11,8 +11,22 @@ import (
 const (
 	AUTH_TOKEN_IDENTIFIER = "SLACK_AUTH_TOKEN"
 	CHANNEL_ID_IDENTIFIER = "SLACK_CHANNEL_ID"
-	USER_INFO_TEMPLATE    = ""
 )
+
+const (
+	CLUSTER_ROLE_REQUEST_MADE = "kubectl list %s"
+	ROLE_REQUEST_MADE         = "kubectl list %s %s"
+	TENANT_REQUEST_MADE       = "kubectl list %s "
+)
+
+// // "clusterrole-request-made"
+// // "role-request-made"
+// // "tenant-request-made"
+// const (
+// 	CLUSTER_ROLE_REQUEST_MADE = "kubectl patch clusterrolerequest %s --type='json' -p='[{\"op\": \"replace\", \"path\": \"/spec/approved\", \"value\":true}]' --kubeconfig ./edgenet-kubeconfig.cfg"
+// 	ROLE_REQUEST_MADE   =        "kubectl patch rolerequest {{.CommonData.Username}} -n {{.CommonData.Namespace}} --type='json' -p='[{\"op\": \"replace\", \"path": \"/spec/approved\", \"value\":true}]' --kubeconfig ./edgenet-kubeconfig.cfg"
+// 	TENANT_REQUEST_MADE =      "kubectl patch tenantrequest {{.CommonData.Tenant}} --type='json' -p='[{\"op\": \"replace\", \"path\": \"/spec/approved\", \"value\":true}]' --kubeconfig ./admin.cfg"
+// )
 
 type Content struct {
 	Cluster   string
@@ -25,13 +39,20 @@ type Content struct {
 	// There are not recipient since this the notification will only be sent to a channel.
 	// If requested this may be changed to support multiple Slack channels.
 	// Recipient     []string
-	RoleRequest   *RoleRequest
-	TenantRequest *TenantRequest
+	ClusterRolerequest *ClusterRoleRequest
+	RoleRequest        *RoleRequest
+	TenantRequest      *TenantRequest
 }
+
+type ClusterRoleRequest struct {
+	Name string
+}
+
 type RoleRequest struct {
 	Name      string
 	Namespace string
 }
+
 type TenantRequest struct {
 	Tenant string
 }
@@ -41,28 +62,49 @@ func (c *Content) Send(purpose string) error {
 
 	googleScholarLink := fmt.Sprintf("<https://scholar.google.com/scholar?hl=en&as_sdt=0%%2C5&q=%s+%s&oq=|Google Scholar>", c.FirstName, c.LastName)
 
+	fields := []slack.AttachmentField{
+		{
+			Title: "User Information",
+			Value: fmt.Sprintf("%s (%s %s) Google Scholar: %s", c.User, c.FirstName, c.LastName, googleScholarLink),
+		},
+		{
+			Title: "Cluster Information",
+			Value: c.Cluster,
+		},
+		{
+			Title: "Date",
+			Value: time.Now().String(),
+		},
+		{
+			Title: "Console Link",
+			Value: "Use this <https://www.edge-net.org/|link>",
+		},
+	}
+
+	// If the command for approval exists also add it to the slack notification
+	if purpose == "clusterrole-request-made" {
+		fields = append(fields, slack.AttachmentField{
+			Title: "Console Command",
+			Value: fmt.Sprintf(CLUSTER_ROLE_REQUEST_MADE, c.ClusterRolerequest.Name),
+		})
+	} else if purpose == "rolerequest-made" {
+		fields = append(fields, slack.AttachmentField{
+			Title: "Console Command",
+			Value: fmt.Sprintf(ROLE_REQUEST_MADE, c.RoleRequest.Name, c.RoleRequest.Namespace),
+		})
+	} else if purpose == "tenant-request-made" {
+		fields = append(fields, slack.AttachmentField{
+			Title: "Console Command",
+			Value: fmt.Sprintf(TENANT_REQUEST_MADE, c.TenantRequest.Tenant),
+		})
+	}
+
+	// Set edgenet colors
 	attachment := slack.Attachment{
 		Pretext: c.Subject,
 		Text:    purpose,
 		Color:   "#3e7fb8",
-		Fields: []slack.AttachmentField{
-			{
-				Title: "User Information",
-				Value: fmt.Sprintf("%s (%s %s) Google Scholar: %s", c.User, c.FirstName, c.LastName, googleScholarLink),
-			},
-			{
-				Title: "Cluster Information",
-				Value: c.Cluster,
-			},
-			{
-				Title: "Date",
-				Value: time.Now().String(),
-			},
-			{
-				Title: "Console Link",
-				Value: "Use this <https://www.edge-net.org/|link>",
-			},
-		},
+		Fields:  fields,
 	}
 
 	_, timestamp, err := client.PostMessage(
