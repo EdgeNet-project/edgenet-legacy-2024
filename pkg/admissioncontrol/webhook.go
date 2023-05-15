212db21c837536632d0ecc68bbac0ee6b55423ea
bsenel <beratcansenel@gmail.com>
2023-05-15 15:36:40 +0200

    Update admission control for quickfix

--- pkg/admissioncontrol/webhook.go
+++ pkg/admissioncontrol/webhook.go
@@ -529,10 +529,12 @@ func (wh *Webhook) validateSubNamespace(w http.ResponseWriter, r *http.Request) 
 			}
 		}
 
-		if *oldSubnamespace.GetSliceClaim() != *subnamespace.GetSliceClaim() {
-			admissionResponse.Allowed = false
-			admissionResponse.Result = &metav1.Status{
-				Message: "subsidiary namespace slice cannot be set after creation",
+		if oldSubnamespace.GetSliceClaim() != nil && subnamespace.GetSliceClaim() != nil {
+			if *oldSubnamespace.GetSliceClaim() != *subnamespace.GetSliceClaim() {
+				admissionResponse.Allowed = false
+				admissionResponse.Result = &metav1.Status{
+					Message: "subsidiary namespace slice cannot be set after creation",
+				}
 			}
 		}
 
