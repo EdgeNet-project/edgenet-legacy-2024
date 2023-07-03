f43e7c7c5fb8cd7e5625fca6f814961612a92fba
ubombar <ufukbombar@gmail.com>
2023-07-03 17:44:52 +0200

    Add the base version of fedmanctl commands

--- pkg/fedmanctl/cmd/manager.go
+++ pkg/fedmanctl/cmd/manager.go
@@ -11,6 +11,11 @@ var managerCmd = &cobra.Command{
 	Use:     "manager",
 	Aliases: []string{"m"},
 	Short:   "Manage manager clusters",
+}
+
+var managerLinkCmd = &cobra.Command{
+	Use:   "link",
+	Short: "Link a worker cluster with the generated token.",
 	Run: func(cmd *cobra.Command, args []string) {
 		f, err := fedmanctl.NewFedmanctl(kubeconfig, context)
 
@@ -18,10 +23,40 @@ var managerCmd = &cobra.Command{
 			panic(err.Error())
 		}
 
-		fmt.Printf("%v\n", f.Version())
+		fmt.Printf("Not Implemented command of fedmanctl version: %v\n", f.Version())
+	},
+}
+
+var managerUnlinkCmd = &cobra.Command{
+	Use:   "unlink",
+	Short: "Unlink a worker cluster with the uid.",
+	Run: func(cmd *cobra.Command, args []string) {
+		f, err := fedmanctl.NewFedmanctl(kubeconfig, context)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Printf("Not Implemented command of fedmanctl version: %v\n", f.Version())
+	},
+}
+
+var managerListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all of the federated clusters.",
+	Run: func(cmd *cobra.Command, args []string) {
+		f, err := fedmanctl.NewFedmanctl(kubeconfig, context)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Printf("Not Implemented command of fedmanctl version: %v\n", f.Version())
 	},
 }
 
 func init() {
-
+	managerCmd.AddCommand(managerLinkCmd)
+	managerCmd.AddCommand(managerUnlinkCmd)
+	managerCmd.AddCommand(managerListCmd)
 }
--- pkg/fedmanctl/cmd/worker.go
+++ pkg/fedmanctl/cmd/worker.go
@@ -1,16 +1,62 @@
 package fedmanctl
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/EdgeNet-project/edgenet/pkg/fedmanctl"
+	"github.com/spf13/cobra"
+)
 
 var workerCmd = &cobra.Command{
 	Use:     "worker",
 	Aliases: []string{"w"},
 	Short:   "Manage worker clusters",
-	Run: func(cmd *cobra.Command, args []string) {
+}
 
+var workerInitCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize the cluster as the worker cluster. Configure such that, it can receive and send workloads.",
+	Run: func(cmd *cobra.Command, args []string) {
+		f, err := fedmanctl.NewFedmanctl(kubeconfig, context)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Printf("Not Implemented command of fedmanctl version: %v\n", f.Version())
+	},
+}
+
+var workerResetCmd = &cobra.Command{
+	Use:   "reset",
+	Short: "Reset the configuration of the cluster as a workload cluster.",
+	Run: func(cmd *cobra.Command, args []string) {
+		f, err := fedmanctl.NewFedmanctl(kubeconfig, context)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Printf("Not Implemented command of fedmanctl version: %v\n", f.Version())
+	},
+}
+
+var workerTokenCmd = &cobra.Command{
+	Use:   "token",
+	Short: "Generate the token to be fed to the manager cluster. Geo tags can be overwritten.",
+	Run: func(cmd *cobra.Command, args []string) {
+		f, err := fedmanctl.NewFedmanctl(kubeconfig, context)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Printf("Not Implemented command of fedmanctl version: %v\n", f.Version())
 	},
 }
 
 func init() {
-
+	workerCmd.AddCommand(workerInitCmd)
+	workerCmd.AddCommand(workerResetCmd)
+	workerCmd.AddCommand(workerTokenCmd)
 }
